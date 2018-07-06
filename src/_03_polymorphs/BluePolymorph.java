d50607ea3e4ebfdc1d31e168bff5956b6dc81449
BenProgramsThings <league@WTS14.attlocal.net>
2018-07-05 19:48:25 -0700

    finished parts 1 and 2

--- src/_03_polymorphs/BluePolymorph.java
+++ src/_03_polymorphs/BluePolymorph.java
@@ -3,7 +3,25 @@ package _03_polymorphs;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class BluePolymorph extends Polymorph{
+public class BluePolymorph extends Polymorph {
+	protected int width = 50;
+	protected int height = 50;
+
+	protected int getwidth() {
+		return width;
+	}
+
+	protected int getheight() {
+		return height;
+	}
+
+	void setwidth() {
+		this.width = width;
+	}
+
+	void setheight() {
+		this.height = height;
+	}
 
 	BluePolymorph(int x, int y) {
 		super(x, y);
@@ -12,7 +30,7 @@ public class BluePolymorph extends Polymorph{
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.blue);
-		g.fillRect(x, y, 50, 50);
+		g.fillRect(x, y, width, height);
 	}
-	
+
 }
